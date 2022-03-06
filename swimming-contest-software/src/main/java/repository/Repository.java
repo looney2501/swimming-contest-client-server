4b65b81fafbac46c8b756fd2deb9e513d67b5a88
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-06 23:08:22 +0200

    Create Repository Interface

---
+++ swimming-contest-software/src/main/java/repository/Repository.java
@@ -0,0 +1,17 @@
+package repository;
+
+import java.util.Collection;
+
+/**
+ * Abstract interface for repository objects.
+ * @param <Tid> Type of ID of the objects stored in repository
+ * @param <T> Type of the objects stored in repository
+ */
+public interface Repository<Tid, T> {
+    void add(T elem);
+    void delete(T elem);
+    void update(T elem, Tid id);
+    T findById(Tid id);
+    Iterable<T> findAll();
+    Collection<T> getAll();
+}

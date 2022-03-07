029f869780032dee255241d8ba3dd6a2707b7277
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-07 21:39:57 +0200

    Create Repository interface. Reorganize code in src directory

---
+++ MPP-lab-project/src/Repository/Repository.cs
@@ -0,0 +1,9 @@
+﻿namespace MPP_lab_project.Repository;
+
+public interface Repository<Tid, T>
+{
+    void Add(T elem);
+    void Delete(T elem);
+    void Update(T elem, Tid id);
+    T FindById(Tid id);
+}

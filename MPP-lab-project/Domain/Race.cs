c174592c596611b9beee284fadd9440a759b129f
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-07 21:14:58 +0200

    Create Race class

---
+++ MPP-lab-project/Domain/Race.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MPP_lab_project.Domain;
+
+[Serializable()]
+public class Race: Identifiable<Int32>
+{
+    public int ID { get; set; }
+    public SwimmingDistances Distance { get; set; }
+    public SwimmingStyles Style { get; set; }
+    public Int32 SwimmersNumber { get; set; }
+    
+    
+}

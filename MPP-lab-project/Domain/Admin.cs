9d09a78ceb418f5f889c3d6d7b6ecea51656d3d3
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-07 21:12:26 +0200

    Create Admin class

---
+++ MPP-lab-project/Domain/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MPP_lab_project.Domain;
+
+[Serializable()]
+public class Admin: Identifiable<Int32>
+{
+    public Int32 ID { get; set; }
+    public String username { get; set; }
+    public String Password { get; set; }
+
+    public Admin(int id, string username, string password)
+    {
+        ID = id;
+        this.username = username;
+        Password = password;
+    }
+
+    public Admin(string username, string password)
+    {
+        this.username = username;
+        Password = password;
+    }
+}

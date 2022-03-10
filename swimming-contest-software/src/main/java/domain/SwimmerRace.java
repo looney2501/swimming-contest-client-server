ec64c7b31573f61ee1901a7c0f38b2b55bb95628
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-10 17:01:39 +0200

    Add id to SwimmerRace class and make it implement Identifiable interface

--- swimming-contest-software/src/main/java/domain/SwimmerRace.java
+++ swimming-contest-software/src/main/java/domain/SwimmerRace.java
@@ -1,12 +1,25 @@
 package domain;
 
-public class SwimmerRace {
+import java.io.Serializable;
+
+public class SwimmerRace implements Identifiable<Integer>, Serializable {
+    private Integer ID;
     private final Swimmer swimmer;
     private final Race race;
 
     public SwimmerRace(Swimmer swimmer, Race race) {
         this.swimmer = swimmer;
         this.race = race;
+    }
+
+    @Override
+    public Integer getID() {
+        return ID;
+    }
+
+    @Override
+    public void setID(Integer id) {
+        this.ID = id;
     }
 
     public Swimmer getSwimmer() {

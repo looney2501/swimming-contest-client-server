73bf71d1bef0b6f97e88e409f104d54d5368bf7e
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-11 23:13:00 +0200

    Create converter methods for enums used in Race class

--- MPP-lab-project/src/Domain/SwimmingDistances.cs
+++ MPP-lab-project/src/Domain/SwimmingDistances.cs
@@ -1,4 +1,6 @@
-﻿namespace MPP_lab_project.Domain;
+﻿using System;
+
+namespace MPP_lab_project.Domain;
 
 public enum SwimmingDistances
 {
@@ -6,4 +8,27 @@ public enum SwimmingDistances
     _200,
     _800,
     _1500
+}
+
+public static class SwimmingDistancesMethods
+{
+    public static SwimmingDistances DistanceFromInteger(Int32 value) =>
+        value switch 
+        {
+            1 => SwimmingDistances._50,
+            2 => SwimmingDistances._200,
+            3 => SwimmingDistances._800,
+            4 => SwimmingDistances._1500,
+            _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+        };
+
+    public static Int32 IntegerFromDistance(SwimmingDistances swimmingDistances) =>
+        swimmingDistances switch
+        {
+            SwimmingDistances._50 => 1,
+            SwimmingDistances._200 => 2,
+            SwimmingDistances._800 => 3,
+            SwimmingDistances._1500 => 4,
+            _ => throw new ArgumentOutOfRangeException(nameof(swimmingDistances), swimmingDistances, null)
+        };
 }
--- MPP-lab-project/src/Domain/SwimmingStyles.cs
+++ MPP-lab-project/src/Domain/SwimmingStyles.cs
@@ -1,4 +1,6 @@
-﻿namespace MPP_lab_project.Domain;
+﻿using System;
+
+namespace MPP_lab_project.Domain;
 
 public enum SwimmingStyles
 {
@@ -7,3 +9,27 @@ public enum SwimmingStyles
     Butterfly,
     Backstroke
 }
+
+public static class SwimmingStylesMethods
+{
+    public static SwimmingStyles StyleFromInteger(Int32 value) =>
+        value switch
+        {
+            1 => SwimmingStyles.Mixed,
+            2 => SwimmingStyles.Freestyle,
+            3 => SwimmingStyles.Butterfly,
+            4 => SwimmingStyles.Backstroke,
+            _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+        };
+
+    public static Int32 IntegerFromStyle(SwimmingStyles style) =>
+        style switch
+        {
+            SwimmingStyles.Mixed => 1,
+            SwimmingStyles.Freestyle => 2,
+            SwimmingStyles.Butterfly => 3,
+            SwimmingStyles.Backstroke => 4,
+            _ => throw new ArgumentOutOfRangeException(nameof(style), style, null)
+        };
+}
+

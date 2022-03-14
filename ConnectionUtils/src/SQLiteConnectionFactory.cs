98a7c56ca2a9318f10278cc9412479723dd9f3cc
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-14 19:57:57 +0200

    Create DB factory and DB Utils

---
+++ ConnectionUtils/src/SQLiteConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using log4net.Util;
+
+namespace ConnectionUtils
+{
+    public class SQLiteConnectionFactory : ConnectionFactory
+    {
+        public override IDbConnection CreateConnection(IDictionary<string, string> props)
+        {
+            String connectionString = props["ConnectionString"];
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
---
+++ MPP-lab-project/src/Utils/DBUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using log4net;
+using log4net.Repository.Hierarchy;
+
+namespace MPP_lab_project.Utils;
+
+public static class DBUtils
+{
+    private static IDbConnection _instance = null;
+    private static ILog _logger = LogManager.GetLogger("DBUtils");
+    
+    public static IDbConnection GetConnection(IDictionary<string,string> props)
+    {
+        if (_instance == null || _instance.State == System.Data.ConnectionState.Closed)
+        {
+            String url = props["url"];
+            String user = props["user"];
+            String password = props["password"];
+            
+            _logger.InfoFormat("trying to connect to database ... {0}", url);
+            _logger.InfoFormat("user: {0}", user);
+            _logger.InfoFormat("password: {0}", password);
+            
+            _instance = GetNewConnection(props);
+            _instance.Open();
+            if (_instance.State != ConnectionState.Open)
+            {
+                _logger.Error("Connection could not be established");
+            }
+        }
+        return _instance;
+    }
+
+    private static IDbConnection GetNewConnection(IDictionary<string,string> props)
+    {
+        return ConnectionUtils.ConnectionFactory.GetInstance().CreateConnection(props);
+    }
+}

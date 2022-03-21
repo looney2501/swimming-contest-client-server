ca642f6bb8a7b69f36beebed6786b3db411b67af
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-21 17:41:27 +0200

    View all races and the number of swimmers enrolled

--- swimming-contest-software/src/main/java/swimmingApp/controller/MainController.java
+++ swimming-contest-software/src/main/java/swimmingApp/controller/MainController.java
@@ -1,30 +1,56 @@
 package swimmingApp.controller;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.util.Callback;
 import swimmingApp.Main;
+import swimmingApp.domain.dtos.RaceDTO;
 
 import java.io.IOException;
 
 
 public class MainController extends Controller {
+
     @FXML
     private Button logoutButton;
     @FXML
-    private TableView racesTableView;
+    private TableView<RaceDTO> racesTableView;
     @FXML
-    TableColumn raceNumberColumn;
+    TableColumn<RaceDTO, String> raceNumberColumn;
     @FXML
-    TableColumn raceDistanceColumn;
+    TableColumn<RaceDTO, String> raceDistanceColumn;
     @FXML
-    TableColumn raceStyleColumn;
+    TableColumn<RaceDTO, String> raceStyleColumn;
     @FXML
-    TableColumn raceSwimmersNoColumn;
+    TableColumn<RaceDTO, String> raceSwimmersNoColumn;
+    @FXML
+    ObservableList<RaceDTO> raceDTOsModel = FXCollections.observableArrayList();
 
     @FXML
     public void logoutButtonAction() throws IOException {
         Main.changeSceneToLogin();
+    }
+
+    public void initialize() {
+        initializeRacesModel();
+        initializeRacesTableView();
+    }
+
+    private void initializeRacesTableView() {
+        racesTableView.setItems(raceDTOsModel);
+        raceNumberColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getRace().getID().toString()));
+        raceDistanceColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getRace().getDistance().toString()));
+        raceStyleColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getRace().getStyle().toString()));
+        raceSwimmersNoColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getNoSwimmers().toString()));
+    }
+
+    private void initializeRacesModel() {
+        raceDTOsModel.setAll(service.findAllRacesDetails());
     }
 }

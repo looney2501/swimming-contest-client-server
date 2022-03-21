af684f43d8734f711f8bc55ea7e9a7d2e4fc11b7
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-21 17:51:17 +0200

    Add search for race combo boxes

--- swimming-contest-software/src/main/java/swimmingApp/controller/MainController.java
+++ swimming-contest-software/src/main/java/swimmingApp/controller/MainController.java
@@ -6,19 +6,30 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.util.Callback;
 import swimmingApp.Main;
+import swimmingApp.domain.SwimmingDistances;
+import swimmingApp.domain.SwimmingStyles;
 import swimmingApp.domain.dtos.RaceDTO;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainController extends Controller {
 
     @FXML
     private Button logoutButton;
+    @FXML
+    private Button searchSwimmersButton;
+    @FXML
+    private ComboBox<SwimmingDistances> distanceComboBox;
+    @FXML
+    private ComboBox<SwimmingStyles> styleComboBox;
     @FXML
     private TableView<RaceDTO> racesTableView;
     @FXML
@@ -31,6 +42,10 @@ public class MainController extends Controller {
     TableColumn<RaceDTO, String> raceSwimmersNoColumn;
     @FXML
     ObservableList<RaceDTO> raceDTOsModel = FXCollections.observableArrayList();
+    @FXML
+    ObservableList<SwimmingDistances> distances = FXCollections.observableArrayList();
+    @FXML
+    ObservableList<SwimmingStyles> styles = FXCollections.observableArrayList();
 
     @FXML
     public void logoutButtonAction() throws IOException {
@@ -38,8 +53,9 @@ public class MainController extends Controller {
     }
 
     public void initialize() {
-        initializeRacesModel();
+        initializeModels();
         initializeRacesTableView();
+        initializeComboBoxes();
     }
 
     private void initializeRacesTableView() {
@@ -50,7 +66,14 @@ public class MainController extends Controller {
         raceSwimmersNoColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getNoSwimmers().toString()));
     }
 
-    private void initializeRacesModel() {
+    private void initializeModels() {
         raceDTOsModel.setAll(service.findAllRacesDetails());
+        distances.setAll(Arrays.asList(SwimmingDistances._50, SwimmingDistances._200, SwimmingDistances._800, SwimmingDistances._1500));
+        styles.setAll(Arrays.asList(SwimmingStyles._FREESTYLE, SwimmingStyles._BACKSTROKE, SwimmingStyles._BUTTERFLY, SwimmingStyles._MIXED));
+    }
+
+    private void initializeComboBoxes() {
+        distanceComboBox.setItems(distances);
+        styleComboBox.setItems(styles);
     }
 }

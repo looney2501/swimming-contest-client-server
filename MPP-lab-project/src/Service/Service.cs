1053f136a01850b3db7c0e384e999c6c3c7b6463
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-26 11:50:38 +0200

    Add service

--- MPP-lab-project/src/Service/Service.cs
+++ MPP-lab-project/src/Service/Service.cs
@@ -1,4 +1,8 @@
-﻿using MPP_lab_project.Repository.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using MPP_lab_project.Domain;
+using MPP_lab_project.Domain.DTOs;
+using MPP_lab_project.Repository.Interfaces;
 
 namespace MPP_lab_project.Service;
 
@@ -15,5 +19,48 @@ public class Service
         SwimmerRepository = swimmerRepository;
         RaceRepository = raceRepository;
         SwimmerRaceRepository = swimmerRaceRepository;
+    }
+
+    public bool IsExistingUser(string username, string pasword)
+    {
+        return true;
+    }
+
+    public List<RaceDTO> FindAllRacesDetails()
+    {
+        List<RaceDTO> raceDTOs = new List<RaceDTO>();
+        foreach (Race race in RaceRepository.FindAllRaces())
+        {
+            raceDTOs.Add(new RaceDTO(race, SwimmerRaceRepository.GetNumberOfSwimmersForRace(race)));
+        }
+
+        return raceDTOs;
+    }
+    
+    public List<SwimmerDTO> FindAllSwimmersDetailsForRace(SwimmingDistances swimmingDistance, SwimmingStyles swimmingStyle) {
+        List<SwimmerDTO> swimmerDTos = new List<SwimmerDTO>();
+        Race race = RaceRepository.FindRaceByDistanceAndStyle(swimmingDistance, swimmingStyle);
+        foreach (Swimmer swimmer in SwimmerRaceRepository.FindAllSwimmersForRace(race)) {
+            List<Race> racesForSwimmer = SwimmerRaceRepository.FindAllRacesForSwimmer(swimmer);
+            List<RaceDetailsDTO> raceDetailsDTOS =
+                racesForSwimmer.Select(x => new RaceDetailsDTO(x.Distance, x.Style)).ToList();
+            swimmerDTos.Add(new SwimmerDTO(swimmer, raceDetailsDTOS));
+        }
+        return swimmerDTos;
+    }
+
+    public void AddSwimmer(string fistName, string lastName, int age, List<RaceDetailsDTO> raceDetailsDTOs)
+    {
+        Swimmer swimmer = new Swimmer(fistName, lastName, age);
+        int swimmerID = SwimmerRepository.Add(swimmer);
+        swimmer.ID = swimmerID;
+
+        foreach (RaceDetailsDTO raceDetailDTO in raceDetailsDTOs)
+        {
+            Race race = RaceRepository.FindRaceByDistanceAndStyle(raceDetailDTO.SwimmingDistance,
+                raceDetailDTO.SwimmingStyle);
+            SwimmerRace swimmerRace = new SwimmerRace(swimmer, race);
+            SwimmerRaceRepository.Add(swimmerRace);
+        }
     }
 }

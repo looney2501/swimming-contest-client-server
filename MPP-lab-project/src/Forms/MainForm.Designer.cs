7985af88c7d50aaca881473e31160c8fb9db65f3
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-27 12:08:46 +0300

    Create log out functionality

--- MPP-lab-project/src/Forms/MainForm.Designer.cs
+++ MPP-lab-project/src/Forms/MainForm.Designer.cs
@@ -31,11 +31,44 @@ partial class MainForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
+        this.exitButton = new System.Windows.Forms.Button();
+        this.logoutButton = new System.Windows.Forms.Button();
+        this.SuspendLayout();
+        // 
+        // exitButton
+        // 
+        this.exitButton.Location = new System.Drawing.Point(788, 512);
+        this.exitButton.Name = "exitButton";
+        this.exitButton.Size = new System.Drawing.Size(87, 48);
+        this.exitButton.TabIndex = 0;
+        this.exitButton.Text = "Exit";
+        this.exitButton.UseVisualStyleBackColor = true;
+        this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+        // 
+        // logoutButton
+        // 
+        this.logoutButton.Location = new System.Drawing.Point(695, 512);
+        this.logoutButton.Name = "logoutButton";
+        this.logoutButton.Size = new System.Drawing.Size(87, 48);
+        this.logoutButton.TabIndex = 1;
+        this.logoutButton.Text = "Logout";
+        this.logoutButton.UseVisualStyleBackColor = true;
+        this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+        // 
+        // MainForm
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.ClientSize = new System.Drawing.Size(887, 572);
+        this.Controls.Add(this.logoutButton);
+        this.Controls.Add(this.exitButton);
+        this.Name = "MainForm";
         this.Text = "MainForm";
+        this.ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Button exitButton;
+    private System.Windows.Forms.Button logoutButton;
 
     #endregion
 }
--- MPP-lab-project/src/Forms/MainForm.cs
+++ MPP-lab-project/src/Forms/MainForm.cs
@@ -1,11 +1,25 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace MPP_lab_project.Forms;
 
 public partial class MainForm : GenericForm
 {
+    private Service.Service _service;
     public MainForm(Service.Service service) : base(service)
     {
+        _service = service;
         InitializeComponent();
+    }
+
+    private void exitButton_Click(object sender, EventArgs e)
+    {
+        Application.Exit();
+    }
+
+    private void logoutButton_Click(object sender, EventArgs e)
+    {
+        Close();
+        new LoginForm(service).Show();
     }
 }

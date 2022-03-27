619631ba297054766be241639f29c823b69b5296
Mircea-George Cozarev <mircea2501@gmail.com>
2022-03-27 12:06:14 +0300

    Solve GenericForm bug

--- MPP-lab-project/src/Forms/GenericForm.Designer.cs
+++ MPP-lab-project/src/Forms/GenericForm.Designer.cs
@@ -31,10 +31,16 @@ partial class GenericForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
+        this.SuspendLayout();
+        // 
+        // GenericForm
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.ClientSize = new System.Drawing.Size(938, 576);
+        this.Name = "GenericForm";
         this.Text = "GenericForm";
+        this.ResumeLayout(false);
     }
 
     #endregion
--- MPP-lab-project/src/Forms/GenericForm.cs
+++ MPP-lab-project/src/Forms/GenericForm.cs
@@ -1,10 +1,17 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace MPP_lab_project.Forms;
 
 public partial class GenericForm : Form
 {
     protected Service.Service service;
+
+    [Obsolete("Designer only", true)]
+    public GenericForm()
+    {
+        InitializeComponent();
+    }
     
     public GenericForm(Service.Service service)
     {

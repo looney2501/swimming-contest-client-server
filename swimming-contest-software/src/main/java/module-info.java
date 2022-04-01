c2c4de063aa3c7c9b7e9229ba8b2198d76390a11
Mircea-George Cozarev <mircea2501@gmail.com>
2022-04-01 18:11:06 +0300

    Solve module-info.java bug

--- swimming-contest-software/src/main/java/module-info.java
+++ swimming-contest-software/src/main/java/module-info.java
@@ -2,9 +2,6 @@ module com.map_toysocialnetwork_gui {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
     requires java.sql;
     requires java.desktop;
     requires org.apache.logging.log4j;
